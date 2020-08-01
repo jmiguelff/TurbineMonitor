10fcbbab7c4bff374d6436f2d705f631247e67a1
Joao Ferreira <jmiguelff@gmail.com>
2020-08-01 22:50:49 +0100

    Add encoder library - still trying to make it work

--- TurbineMonitor/src/main.cpp
+++ TurbineMonitor/src/main.cpp
@@ -5,6 +5,8 @@
 #include <LiquidCrystal_I2C.h>
 #include "RTClib.h"
 #include "DHT.h"
+#include <ClickEncoder.h>
+#include <TimerOne.h>
 
 /*
  * DHT Library init
@@ -39,6 +41,36 @@ const uint8_t bluePin = 6;
 LiquidCrystal_I2C lcd(PCF8574_ADDR_A21_A11_A01, 4, 5, 6, 16, 11, 12, 13, 14, POSITIVE);
 
 /*
+ * Configuration Menu
+ */
+uint8_t menuSize = 10;
+String menu[] = {
+    "Menu",
+    "Warning Top Val",
+    "Warning Bot Val",
+    "Alarm Top Val",
+    "Alarm Bot Val",
+    "Exit",
+    "NA",
+    "NA",
+    "NA",
+    ""
+};
+boolean up = false;
+boolean down = false;
+
+/*
+ * Encoder Library
+ */
+#define ENCODER_PINA 8
+#define ENCODER_PINB 4
+#define ENCODER_BTN A2
+
+ClickEncoder *encoder;
+int16_t last, value;
+uint8_t btnState = 0;
+
+/*
  *  RTC Library 
  */
 RTC_DS3231 rtc;
@@ -59,7 +91,11 @@ long lastReconnectAttempt = 0;
 /*
  * Alarm State Machine Variables
  */
-enum { NORMAL = 0, WARNING, ALARM} state;
+enum State { NORMAL = 0, WARNING, ALARM, MENU};
+
+// Initial state
+State state = NORMAL;
+State lastState = NORMAL;
 
 /*
  * Analog Read Function
@@ -139,6 +175,89 @@ void msg_rcv_callback(char *topic, byte* payload, unsigned int length) {
     Serial.println();
 }
 
+/*
+ * Interrupt routine to get encoder state
+ */
+void timerIsr() {
+    encoder->service();
+}
+
+/*
+ * Get rotary encoder
+ */
+void readRotaryEncoder() {
+    value += encoder->getValue();
+  
+    if (value/2 > last) {
+        last = value/2;
+        down = true;
+        delay(150);
+    } 
+    else if (value/2 < last) {
+        last = value/2;
+        up = true;
+        delay(150);
+    }
+}
+
+/*
+ * Display menu text
+ */
+void displayMenu(uint8_t pos) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print(menu[pos]);
+    lcd.setCursor(0, 1);
+    lcd.print(menu[pos + 1]);
+}
+
+/*
+ * Use menu function
+ */
+void useMenu() {
+    uint8_t currentPos = 0;
+    uint8_t btn = 0;
+    uint8_t exitNow = 0;
+
+    // Call this update screen
+    displayMenu(currentPos);
+
+    while(!exitNow) {
+        readRotaryEncoder();
+        if (up) Serial.println("UP");
+        if (down) Serial.println("DOWN");
+
+        // if (encPos > oldEncPos) {
+        //     if (currentPos > 8) {
+        //         currentPos = 0;
+        //     }
+        //     else {
+        //         currentPos++;
+        //     }
+        //     displayMenu(currentPos);
+        //     oldEncPos = encPos;
+        // }
+        // else if (encPos < oldEncPos) {
+        //     if (currentPos == 0) {
+        //         currentPos = 8;
+        //     }
+        //     else {
+        //         currentPos--;
+        //     }
+        //     displayMenu(currentPos);
+        //     oldEncPos = encPos;
+        // }
+        // else {
+        //     Serial.println("No change");
+        // }
+
+        // Update button value
+
+        // Wait one cycle
+        // delay(1000);
+    }
+}
+
 void setup() {
     // Initialize serial port
     Serial.begin(9600);
@@ -158,6 +277,15 @@ void setup() {
 
     lcd.setCursor(0, 0);
     lcd.print("Booting up");
+
+    // Initialize encoder
+    Timer1.initialize(1000);
+    Timer1.attachInterrupt(timerIsr);
+
+    encoder = new ClickEncoder(ENCODER_PINA, ENCODER_PINB, ENCODER_BTN);
+    encoder->setAccelerationEnabled(false);
+
+    last = encoder->getValue();
 
     // Intialize DHT library
     dht.begin();
@@ -181,9 +309,6 @@ void setup() {
     client.setClient(ethClient);
     client.setServer(server, 1883);
     client.setCallback(msg_rcv_callback);
-
-    // Initial state
-    state = NORMAL;
 
     // Start RTC
     if (!rtc.begin()) {
@@ -218,25 +343,71 @@ void loop() {
     // ADC read logic
     float voltage = averageMeasure(A0, 60);
 
-    // Print read value
-    // Serial.println(voltage);
-
     // Update state
     switch(state) {
     case NORMAL:
-        if (voltage > alarmTopLevel) {state = ALARM; break;}
-        if (voltage > warnTopLevel) {state = WARNING; break;}
-        state = NORMAL; break;
+        if (btnState == ClickEncoder::Clicked) {
+            lastState = state; 
+            state = MENU; 
+            break;
+        }
+        
+        if (voltage > alarmTopLevel) {
+            state = ALARM; 
+            break;
+        }
+        
+        if (voltage > warnTopLevel) {
+            state = WARNING; 
+            break;
+        }
+
+        state = NORMAL; 
+        break;
 
     case WARNING:
-        if (voltage > alarmTopLevel) {state = ALARM; break;}
-        if (voltage < warnBotLevel) {state = NORMAL; break;}
-        state = WARNING; break;
+        if (btnState == ClickEncoder::Clicked) {
+            lastState = state; 
+            state = MENU; 
+            break;
+        }
+
+        if (voltage > alarmTopLevel) {
+            state = ALARM; 
+            break;
+        }
+        
+        if (voltage < warnBotLevel) {
+            state = NORMAL; 
+            break;
+        }
+        
+        state = WARNING; 
+        break;
 
     case ALARM:
-        if (voltage < warnBotLevel) {state = NORMAL; break;}
-        if (voltage < alarmBotLevel) {state = WARNING; break;}
-        state = ALARM; break;
+        if (btnState == ClickEncoder::Clicked) {
+            lastState = state;
+            state = MENU; 
+            break;
+        }
+        
+        if (voltage < warnBotLevel) {
+            state = NORMAL; 
+            break;
+        }
+        
+        if (voltage < alarmBotLevel) {
+            state = WARNING; 
+            break;
+        }
+
+        state = ALARM; 
+        break;
+
+    case MENU:
+        state = lastState;
+        break;
     }
 
     // Update output
@@ -253,11 +424,7 @@ void loop() {
         if (isRTCpresent) {
             DateTime now = rtc.now();
             memset(myTimestamp, 0, sizeof(myTimestamp));
-            sprintf(myTimestamp, "%d/%d-%d:%d:%d", now.day(), 
-                                                   now.month(), 
-                                                   now.hour(), 
-                                                   now.minute(),
-                                                   now.second());
+            sprintf(myTimestamp, "%d/%d-%d:%d:%d", now.day(), now.month(), now.hour(), now.minute(), now.second());
             lcd.clear();
             lcd.setCursor(0,0);
             lcd.print(myTimestamp);
@@ -283,22 +450,29 @@ void loop() {
             client.publish("state", "ALARM");
         }
         break;
+    
+    case MENU:
+        useMenu();
+        break;
     }
 
-    // Reaad Temperature and Humidity (TEST)
-    float h = dht.readHumidity();
-    float t = dht.readTemperature();
+    // Button trigger for menu
+    btnState = encoder->getButton();
 
-    if (isnan(h) || isnan(t)) {
-        Serial.println("Fail to read temperature sensor");
-    }
-    else {
-        Serial.print("Temp [");
-        Serial.print(t);
-        Serial.print(" C] Humidity [");
-        Serial.print(h);
-        Serial.println(" %]");
-    }
+    // Read Temperature and Humidity (TEST)
+    //float h = dht.readHumidity();
+    //float t = dht.readTemperature();
+//
+    //if (isnan(h) || isnan(t)) {
+    //    Serial.println("Fail to read temperature sensor");
+    //}
+    //else {
+    //    Serial.print("Temp [");
+    //    Serial.print(t);
+    //    Serial.print(" C] Humidity [");
+    //    Serial.print(h);
+    //    Serial.println(" %]");
+    //}
 
     // Wait one cycle
     delay(1000);
